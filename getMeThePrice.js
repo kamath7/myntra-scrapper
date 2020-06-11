e3f57ebead24e53517529bdd18cd55734ba4e6d0
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-06-11 14:24:15 +0530

    Few changes to avoid puppeteer crash

--- getMeThePrice.js
+++ getMeThePrice.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const getMeThePrice = async(url)=>{
     try{
-        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+        const browser = await puppeteer.launch({ args: ['--no-sandbox','--disable-setuid-sandbox'] });
         const page = await browser.newPage();
         await page.goto(url)
         const body = await page.evaluate(()=>{
